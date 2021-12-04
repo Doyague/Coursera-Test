41ff4eff56c85d7f4d4b34758cb0314f9ddc0c21
Doyague <93447991+Doyague@users.noreply.github.com>
2021-12-04 18:54:08 +0000

    coursera

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,8 +3,7 @@
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var name in names) {
-    var firstLetter = names[name].charAt(0);
-    firstLetter = firstLetter.toLowerCase();
+    var firstLetter = (names[name].charAt(0)).toLowerCase();
 
     window.name = names[name];
 
